c3aeab2ad0b6bfb9ab06f2a980b4527d5918bec7
Nikolay Nyunin <nikolay.nyunin@gmail.com>
2023-11-30 21:02:31 +0300

    Improve the GUI

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from llm_rs import AutoModel
 import tkinter as tk
 
-def send_text():
-    text_to_copy = entry1.get("1.0", tk.END)
-    entry2.delete("1.0", tk.END)
-    entry2.insert("1.0", model.generate(text_to_copy).text)
+
+def generate_text():
+    prompt = text_input.get("1.0", tk.END)
+    text_output.configure(state='normal')
+    text_output.delete("1.0", tk.END)
+    text_output.insert("1.0", model.generate(prompt).text)
+    text_output.configure(state='disabled')
 
 
 model = AutoModel.from_pretrained("./ruGPT-3.5-13B-ggml", model_file="./ruGPT-3.5-13B-q4_0.bin")
 
 root = tk.Tk()
-root.title("Chat")
+root.title("ruGPT-3.5 13B")
 
-entry1 = tk.Text(root, width=50, height=8)
-entry1.pack()
+text_input = tk.Text(root, width=80, height=8, font="Arial 18", padx=10, pady=10)
+text_input.pack()
 
-entry2 = tk.Text(root, width=50, height=8)
-entry2.pack()
+text_output = tk.Text(root, width=80, height=8, font="Arial 18", padx=10, pady=10, state="disabled")
+text_output.pack()
 
-copy_button = tk.Button(root, text="Send", command=send_text)
-copy_button.pack()
+generate_button = tk.Button(root, text="Generate", command=generate_text, font="Arial 18")
+generate_button.pack()
 
 root.mainloop()
